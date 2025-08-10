ce6e01fc61d44a752f7b90ba5f777befc6876479
Hridaey Raya <hridaeyraya.22@gmail.com>
2025-08-10 22:24:01 +0545

    Initial commit

---
+++ frontend/expense-tracker/src/pages/Auth/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div>
+      SU
+    </div>
+  )
+}
+
+export default SignUp
---
+++ frontend/expense-tracker/src/pages/Dashboard/Income.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Income = () => {
+  return (
+    <div>
+      INcome
+    </div>
+  )
+}
+
+export default Income
