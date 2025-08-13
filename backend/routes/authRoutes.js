1dd757739720b12d7d96c4d526379f3cfb8c01c7
Hridaey Raya <hridaeyraya.22@gmail.com>
2025-08-13 08:41:26 +0545

    Tested API's are here

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 import { registerUser, loginUser, getUserInfo } from "../controllers/authControllers.js";
-import { protect } from "../middleware/authMiddleware.js";
+import {protect} from "../middleware/authMiddleware.js"
 
 const router = express.Router();
 
