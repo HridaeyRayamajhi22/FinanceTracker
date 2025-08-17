c018db9cbed667dd965b76939b9507968aa53c43
Hridaey Raya <hridaeyraya.22@gmail.com>
2025-08-17 23:09:50 +0545

    User information is used

--- frontend/expense-tracker/src/components/layouts/SideMenu.jsx
+++ frontend/expense-tracker/src/components/layouts/SideMenu.jsx
@@ -1,37 +1,43 @@
-import React, { useContext } from 'react'
-import { SIDE_MENU_DATA } from '../../utils/data'
-import { UserContext } from '../../context/UserContext'
-import { useNavigate } from 'react-router-dom'
+import React, { useContext } from "react";
+import { SIDE_MENU_DATA } from "../../utils/data";
+import { UserContext } from "../../context/UserContext";
+import { useNavigate } from "react-router-dom";
+import CharAvatar from "../Cards/CharAvatar"; // make sure to import your CharAvatar component
 
 const SideMenu = ({ activeMenu }) => {
-  const { user, clearUser } = useContext(UserContext)
-  const navigate = useNavigate()
+  const { user, clearUser } = useContext(UserContext);
+  const navigate = useNavigate();
 
   const handleClick = (route) => {
     if (route === "logout") {
-      handleLogout()
-      return
+      handleLogout();
+      return;
     }
-    navigate(route)
-  }
+    navigate(route);
+  };
 
   const handleLogout = () => {
-    localStorage.clear()
-    clearUser()
-    navigate("/login")
-  }
+    localStorage.clear();
+    clearUser();
+    navigate("/login");
+  };
 
   return (
     <div className="w-64 h-[calc(100vh-61px)] bg-white border-r border-gray-200/50 p-5 sticky top-[61px] z-20">
       <div className="flex flex-col items-center gap-3 mt-3 mb-7">
         {user?.profileImageUrl ? (
           <img
-            src={user?.profileImageUrl}
-            alt="Profile"
+            src={user.profileImageUrl}
+            alt="Profile Image"
             className="w-20 h-20 bg-slate-300 rounded-full"
           />
         ) : (
-          <div className="w-20 h-20 bg-slate-300 rounded-full" />
+          <CharAvatar
+            fullName={user?.fullName || "Guest"}
+            width="w-20"
+            height="h-20"
+            style="text-xl"
+          />
         )}
 
         <h5 className="text-gray-950 font-medium leading-6">
@@ -51,7 +57,7 @@ const SideMenu = ({ activeMenu }) => {
         </button>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default SideMenu
+export default SideMenu;
