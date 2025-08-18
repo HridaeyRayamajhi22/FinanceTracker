c13d87c8a5e8a80acd03463bd963dced6d948cd1
Hridaey Raya <hridaeyraya.22@gmail.com>
2025-08-18 22:24:25 +0545

    Income Section updating

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -14,6 +14,7 @@ import FinanceOverview from '../../components/Dashboard/FinanceOverview';
 import ExpenseTransactions from '../../components/Dashboard/ExpenseTransactions';
 import Last30DaysExpenses from '../../components/Dashboard/Last30DaysExpenses';
 import RecentIncomeWithChart from '../../components/Dashboard/RecentIncomeWithChart';
+import RecentIncome from '../../components/Dashboard/RecentIncome';
 
 const Home = () => {
   useUserAuth();
@@ -47,7 +48,7 @@ const Home = () => {
   return (
     <DashboardLayout activeMenu="Dashboard">
       <div className='my-5 mx-auto'>
-        {/* <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
 
           <InfoCard
             icon={<IoCard className="text-white w-8 h-8" />}
@@ -69,10 +70,10 @@ const Home = () => {
             value={addThousandsSeparator(dashboardData?.totalExpense || 0)}
             color="bg-red-500"
           />
-        </div> */}
+        </div>
 
         <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-6'>
-          {/* <RecentTransactions
+          <RecentTransactions
             transactions={dashboardData?.recentTransactions}
             onSeeMore={() => navigate("/expense")}
           />
@@ -81,23 +82,27 @@ const Home = () => {
             totalBalance={dashboardData?.totalBalance || 0}
             totalIncome={dashboardData?.totalIncome || 0}
             totalExpense={dashboardData?.totalExpense || 0}
-          /> */}
+          />
 
-          {/* <ExpenseTransactions
+          <ExpenseTransactions
              transactions={dashboardData?.last30DaysExpenses?.transactions | [] }
              onSeeMore={() => navigate("/expense")}
           />
 
           <Last30DaysExpenses
             data={dashboardData?.last30DaysExpenses?.transactions || []} 
-          /> */}
+          />
 
           <RecentIncomeWithChart 
             data={dashboardData?.last60DaysIncome?.transactions?.slice(0,4) || []}
             totalIncome={dashboardData?.totalIncome || 0 }
           />
 
-          
+          <RecentIncome
+             transactions={dashboardData?.last60DaysIncome?.transactions || []}
+             onSeeMore={() => navigate('/income')}
+             
+            />
         </div>
       </div>
     </DashboardLayout>
--- frontend/expense-tracker/src/pages/Dashboard/Income.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Income.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import DashboardLayout from '../../components/layouts/DashboardLayout'
+import IncomeOverview from '../../components/Income/IncomeOverview'
 
 const Income = () => {
+
+  const [incomeData, setIncomeData] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [openDeleteAlert, setOpenDeleteAlert] = useState({
+      show: false,
+      data:null
+    });
+
+  const [openAddIncomeModel,setOpenAddIncomeModel] = useState(false)
+
+  // GEt All Income Details
+  const fetchIncomeDetails = async () => {};
+
+  // Handle Add Income
+  const handleAddIncome = async (income) => {
+
+  }
+
+  // Handle Download income Details
   return (
-    <div>
-      INcome
-    </div>
+     <DashboardLayout activeMenu="Income">
+      <div className='my-5 mx-auto'>
+        <div className='grid grid-cols-1 gap-6'>
+           <div className=''> 
+            <IncomeOverview
+               transactions={incomeData}
+               onAddIncome={() => setOpenAddIncomeModel(true)}      
+            />
+           </div>
+        </div>
+      </div>
+      </DashboardLayout>
   )
 }
 
